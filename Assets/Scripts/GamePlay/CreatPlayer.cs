b76b651c0014a2dae0e98878b3b15219af9aca5d
zaixsen <ZaiXSen@163.com>
2024-03-26 17:24:56 +0800

    角色移动

--- Assets/Scripts/GamePlay/CreatPlayer.cs
+++ Assets/Scripts/GamePlay/CreatPlayer.cs
@@ -13,6 +13,8 @@ public class CreatPlayer : MonoBehaviour
         MessageCenter<byte[]>.Ins.AddListener(MessageId.SC_GET_ONLINE_PLAYERCALL, GetOnlinePlayer);
         MessageCenter<byte[]>.Ins.AddListener(MessageId.SC_CLOSE_APP_RECALL, OhterPlayerDown);
         MessageCenter<byte[]>.Ins.AddListener(MessageId.SC_PLAYER_MOVE_CALL, OhterPlayerMove);
+
+
     }
 
     private void OhterPlayerMove(byte[] obj)

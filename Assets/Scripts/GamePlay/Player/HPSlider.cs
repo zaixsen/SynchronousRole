0f91a61b79d6fee95198662b0f91e4a90fc60d27
zaixsen <ZaiXSen@163.com>
2024-03-27 14:53:43 +0800

    更新血条同步

---
+++ Assets/Scripts/GamePlay/Player/HPSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayerInfo;
+using Google.Protobuf;
+public class HPSlider : MonoBehaviour
+{
+    GameObject hpSli;
+    Slider slider;
+    Text Hptext;
+    public int hight = 60;
+
+    private void Start()
+    {
+        hpSli = Instantiate(Resources.Load<GameObject>("Prefabs/HpSlider"), GameObject.Find("Hplay").transform);
+        slider = hpSli.GetComponent<Slider>();
+        slider.maxValue = PlayerModel.Ins.myPlayerData.AllHp;
+        slider.value = PlayerModel.Ins.myPlayerData.AllHp;
+        Hptext = hpSli.GetComponentInChildren<Text>();
+        Hptext.text = PlayerModel.Ins.myPlayerData.NowHp + "/" + PlayerModel.Ins.myPlayerData.AllHp;
+    }
+
+    public void SetHp(int hit)
+    {
+        PlayerModel.Ins.myPlayerData.NowHp -= hit;
+        slider.value -= hit;
+        Hptext.text = PlayerModel.Ins.myPlayerData.NowHp + "/" + PlayerModel.Ins.myPlayerData.AllHp;
+        //发送自身信息
+        NetMgr.Ins.AsySend(MessageId.CS_SHOW_PLAYER_HP, PlayerModel.Ins.myPlayerData.ToByteArray());
+    }
+
+    private void Update()
+    {
+        hpSli.transform.position = Camera.main.WorldToScreenPoint(transform.position) + Vector3.up * hight;
+    }
+}
---
+++ Assets/Scripts/GamePlay/Player/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Protobuf;
+public class PlayerMove : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    bool isOne = false;
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 pos = new Vector3(x, 0, y);
+
+        if (pos != Vector3.zero)
+        {
+            animator.SetBool("Move", true);
+            transform.LookAt(transform.position + pos);
+            transform.Translate(Vector3.forward * 4 * Time.deltaTime);
+
+            PlayerModel.Ins.myPlayerData.Posx = transform.position.x;
+            PlayerModel.Ins.myPlayerData.Posz = transform.position.z;
+            PlayerModel.Ins.myPlayerData.Rosy = transform.eulerAngles.y;
+            PlayerModel.Ins.myPlayerData.AniState = PlayerInfo.AniState.Run;
+
+            isOne = true;
+            NetMgr.Ins.AsySend(MessageId.CS_PLAYER_MOVE, PlayerModel.Ins.myPlayerData.ToByteArray());
+        }
+        else if (pos == Vector3.zero && isOne)
+        {
+            PlayerModel.Ins.myPlayerData.AniState = PlayerInfo.AniState.Idle;
+            animator.SetBool("Move", false);
+
+            NetMgr.Ins.AsySend(MessageId.CS_PLAYER_MOVE, PlayerModel.Ins.myPlayerData.ToByteArray());
+            isOne = false;
+        }
+
+
+    }
+}
